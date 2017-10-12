332fc93a71b728880dff259e3c4d2d39f9feba84
Tapio Mattila <tapio.mattila@edu.turkuamk.fi>
2017-10-12 15:30:14 +0300

    Made javadoc comment on App.class

--- date-storage-project/src/main/java/com/tmattila/launcher/App.java
+++ date-storage-project/src/main/java/com/tmattila/launcher/App.java
@@ -8,6 +8,11 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+/***
+ * 
+ * @author tapsa
+ *
+ */
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan({"com.tmattila"})

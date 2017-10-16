85a1214f90ec3f87897e5138b60dc262030212e4
Tapio Mattila <tapio.mattila@edu.turkuamk.fi>
2017-10-16 15:44:41 +0300

    Testing jenkins with secure db. Change made to ui.

--- date-storage-project/src/main/java/com/tmattila/utils/DateStringUtils.java
+++ date-storage-project/src/main/java/com/tmattila/utils/DateStringUtils.java
@@ -2,7 +2,7 @@ package com.tmattila.utils;
 
 public enum DateStringUtils {
 
-	HEADER_TEXT("Welcome to date storage application, TEST TEST"),
+	HEADER_TEXT("Welcome to date storage application, TEST SECURE DB"),
 	BUTTON_TEXT("Click to save the time of button press to database"),
 	DATE_TEXT("Date and time of button press: ");
 	

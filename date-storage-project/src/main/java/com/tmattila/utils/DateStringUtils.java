3899b78007e9833a53a77ac22d19608a47019d68
Tapio Mattila <tapio.mattila@edu.turkuamk.fi>
2017-10-28 22:42:45 +0300

    JIR-1 #comment Changed the layout, to test #time 5m #done

--- date-storage-project/src/main/java/com/tmattila/utils/DateStringUtils.java
+++ date-storage-project/src/main/java/com/tmattila/utils/DateStringUtils.java
@@ -7,7 +7,7 @@ package com.tmattila.utils;
  */
 public enum DateStringUtils {
 
-	HEADER_TEXT("Welcome to date storage application TEST 28.10"),
+	HEADER_TEXT("Welcome to date storage application TEST 28 JIRA SMART"),
 	BUTTON_TEXT("Click to save the time of button press to database"),
 	DATE_TEXT("Date and time of button press: ");
 	

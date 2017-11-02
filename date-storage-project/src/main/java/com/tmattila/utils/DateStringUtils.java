d2fc6025a737a0700f6f8e6f42a5026113c9b1ce
Tapio Mattila <tapio.mattila@edu.turkuamk.fi>
2017-11-02 20:42:36 +0200

    GRTP-10 #comment This is a test for sharded db #time 5m

--- date-storage-project/src/main/java/com/tmattila/utils/DateStringUtils.java
+++ date-storage-project/src/main/java/com/tmattila/utils/DateStringUtils.java
@@ -7,7 +7,7 @@ package com.tmattila.utils;
  */
 public enum DateStringUtils {
 
-	HEADER_TEXT("Welcome to date storage application TEST GRTP-10 smart commit"),
+	HEADER_TEXT("Welcome to date storage application TEST GRTP-10 sharded db"),
 	BUTTON_TEXT("Click to save the time of button press to database"),
 	DATE_TEXT("Date and time of button press: ");
 	
